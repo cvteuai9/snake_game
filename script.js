44612c9ed9fc39e2a2c5aad49fb667f0a9dcc2de
Cloud <cvteyai9@gmail.com>
2024-12-06 15:15:02 +0800

    遊戲功能開發
    1.貪食蛇移動
    2.食物產生
    3.遊戲開始跳轉畫面

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Define HTML elements
 const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
 
 // Define game variables
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
 
 // Draw game map, snake, food
 function draw() {
@@ -36,7 +42,7 @@ function setPosition(element, position) {
 }
 
 // Testing draw function
-draw();
+// draw();
 
 // Draw food function
 function drawFood() {
@@ -51,3 +57,87 @@ function generateFood() {
   const y = Math.floor(Math.random() * gridSize) + 1;
   return { x, y };
 }
+
+// Moving the snake
+function move() {
+  const head = { ...snake[0] }; // 抓取蛇首的那一個位置
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "right":
+      head.x++;
+      break;
+  }
+  snake.unshift(head); // 將新位置加入蛇首位置
+
+  // 如果吃到食物：
+  // 1.重新產生食物位置
+  // 2.清除計時器(暫停自動移動一下子)
+  // 3.重新設定計時器
+  //   a.執行自動移動函式(蛇首的位置為移動1格後)
+  //   b.執行draw() -> 畫面產生出snake和food(因為沒有執行pop，所以snake長度會+1)
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    increaseSpeed(); // 增加貪食蛇移動速度
+    clearInterval(); // Clear past interval
+    gameInterval = setInterval(() => {
+      move();
+      // checkCollision()
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// // Test moving
+// setInterval(() => {
+//   move(); // Move first
+//   draw(); // Then draw again new position
+// }, 200);
+
+// Start game function
+function startGame() {
+  gameStarted = true; // Keep track of a running game
+  instructionText.style.display = "none";
+  logo.style.display = "none";
+  gameInterval = setInterval(() => {
+    move();
+    // checkCollision()
+    draw();
+  }, gameSpeedDelay);
+}
+
+// Keypress event listener
+function handleKeyPress(event) {
+  if (
+    (!gameStarted && event.code === "Space") ||
+    (!gameStarted && event.key === " ")
+  ) {
+    startGame();
+  } else {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "up";
+        break;
+      case "ArrowDown":
+        direction = "down";
+        break;
+      case "ArrowLeft":
+        direction = "left";
+        break;
+      case "ArrowRight":
+        direction = "right";
+        break;
+    }
+  }
+}
+
+document.addEventListener("keydown", handleKeyPress);
