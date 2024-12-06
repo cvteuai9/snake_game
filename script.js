8ce735667edec8782c346ba7aba9c832813ce4dc
Cloud <cvteyai9@gmail.com>
2024-12-07 01:29:36 +0800

    遊戲開發
    1.貪食蛇移動速度機制
    2.貪食蛇碰撞機制
    3.目前分數、最高分數處理
    4.遊戲重置
    5.遊戲結束畫面處理

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// Define HTML elements
+// 定義HTML上面的元素
 const board = document.getElementById("game-board");
 const instructionText = document.getElementById("instruction-text");
 const logo = document.getElementById("logo");
+const score = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
 
-// Define game variables
+// 定義遊戲變數
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let highScore = 0;
 let direction = "right";
 let gameInterval;
 let gameSpeedDelay = 200;
 let gameStarted = false;
 
-// Draw game map, snake, food
+// 遊戲畫面繪製函式
 function draw() {
   board.innerHTML = "";
   drawSnake();
   drawFood();
+  updateScore();
 }
 
-// Draw snake
+// 製作蛇
 function drawSnake() {
   snake.forEach((segment) => {
     const snakeElement = createGameElement("div", "snake");
@@ -28,14 +32,14 @@ function drawSnake() {
   });
 }
 
-// Create a snake or food cube/div
+// 製作食物
 function createGameElement(tag, className) {
   const element = document.createElement(tag);
   element.className = className;
   return element;
 }
 
-// Set the position of snake or food
+// 設定蛇首或食物位置
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
@@ -46,9 +50,11 @@ function setPosition(element, position) {
 
 // Draw food function
 function drawFood() {
-  const foodElement = createGameElement("div", "food");
-  setPosition(foodElement, food);
-  board.appendChild(foodElement);
+  if (gameStarted) {
+    const foodElement = createGameElement("div", "food");
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+  }
 }
 
 // 隨機產生食物的位置
@@ -86,10 +92,10 @@ function move() {
   if (head.x === food.x && head.y === food.y) {
     food = generateFood();
     increaseSpeed(); // 增加貪食蛇移動速度
-    clearInterval(); // Clear past interval
+    clearInterval(gameInterval); // Clear past interval
     gameInterval = setInterval(() => {
       move();
-      // checkCollision()
+      checkCollision();
       draw();
     }, gameSpeedDelay);
   } else {
@@ -110,7 +116,7 @@ function startGame() {
   logo.style.display = "none";
   gameInterval = setInterval(() => {
     move();
-    // checkCollision()
+    checkCollision();
     draw();
   }, gameSpeedDelay);
 }
@@ -141,3 +147,66 @@ function handleKeyPress(event) {
 }
 
 document.addEventListener("keydown", handleKeyPress);
+
+function increaseSpeed() {
+  if (gameSpeedDelay > 150) {
+    gameSpeedDelay -= 5;
+  } else if (gameSpeedDelay > 100) {
+    gameSpeedDelay -= 3;
+  } else if (gameSpeedDelay > 50) {
+    gameSpeedDelay -= 2;
+  } else if (gameSpeedDelay > 25) {
+    gameSpeedDelay -= 1;
+  }
+}
+
+// 碰撞機制
+function checkCollision() {
+  const head = snake[0];
+  // 邊界碰撞機制
+  if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
+    resetGame();
+  }
+
+  // 蛇蛇本身的碰撞機制
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      resetGame();
+    }
+  }
+}
+
+// 遊戲重置
+function resetGame() {
+  updateHighScore();
+  stopGame();
+  snake = [{ x: 10, y: 10 }];
+  food = generateFood();
+  direction = "right";
+  gameSpeedDelay = 200;
+  updateScore();
+}
+
+// 分數更新
+function updateScore() {
+  const currentScore = snake.length - 1; // 扣掉蛇首
+  score.textContent = currentScore.toString().padStart(3, "0");
+}
+
+// 遊戲結束畫面
+function stopGame() {
+  clearInterval(gameInterval);
+  gameStarted = false;
+  instructionText.style.display = "block";
+  logo.style.display = "block";
+}
+
+// 最高分計算
+function updateHighScore() {
+  const currentScore = snake.length - 1;
+  if (currentScore > highScore) {
+    highScore = currentScore;
+    highScoreText.textContent = highScore.toString().padStart(3, "0");
+  }
+  highScoreText.style.display = "block";
+}
